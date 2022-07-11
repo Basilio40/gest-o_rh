776b46fe6e106dff7abe2d6b0c3a461925b6ce0b
sandrobispo <Kr9zDnciZbhaZ5tWBLkjm3zPRhqj3Vrq+HMLRuPGoYI=>
2022-07-10 21:01:39 -0300

    craiandos os models

--- registro_hora_extra/models.py
+++ registro_hora_extra/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from funcionarios.models import Funcionario
 
 # Create your models here.
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.motivo
