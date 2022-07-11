b48e88786d4a408be8e3964013ce209be4abdf8e
sandrobispo <Kr9zDnciZbhaZ5tWBLkjm3zPRhqj3Vrq+HMLRuPGoYI=>
2022-07-10 21:07:55 -0300

    corrigindo documentos

--- documentos/models.py
+++ documentos/models.py
@@ -5,7 +5,7 @@ from funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
-    proprietario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank= True)
+    proprietario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     
     
     def __str__(self):
